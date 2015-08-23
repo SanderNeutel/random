cf87c1a57d6010b6f835769e30481263fee9d0ac
SanderNeutel <sanderneutel@gmail.com>
2015-08-23 23:12:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #install LXC
-apt-get install -y lxc 
+apt-get -y install lxc 
 
 #create container
 lxc-create -n allspark -t ubuntu 
 
-#set logging
-PROMPT_COMMAND='history -a >(tee -a ~/.bash_history | logger -t " attacker_cmd  $USER[$$] $SSH_CONNECTION")'
 
-$change SSH port
+#change SSH port
 sed -i '/Port 22/c\Port 33968' /etc/ssh/sshd_config
 
-sed "/cdef/aline1\nline2\nline3\nline4" input.txt
+#set logging
+echo 'PROMPT_COMMAND='\'history -a '>(tee -a ~/.bash_history | logger -t " attacker_cmd  $USER[$$] $SSH_CONNECTION")'''\' >>/var/lib/lxc/base/rootfs/etc/bash.bashrc
+
+
