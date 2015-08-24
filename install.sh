9209cf0706009b3ebc8a55b93ec174c696adbe9a
SanderNeutel <sanderneutel@gmail.com>
2015-08-24 21:33:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ lxc-create -n allspark -t ubuntu
 
 #change SSH port
 sed -i '/Port 22/c\Port 33968' /etc/ssh/sshd_config
+service ssh restart
 
 #set logging
 echo 'PROMPT_COMMAND='\'history -a '>(tee -a ~/.bash_history | logger -t " attacker_cmd  $USER[$$] $SSH_CONNECTION")'''\' >>/var/lib/lxc/base/rootfs/etc/bash.bashrc
