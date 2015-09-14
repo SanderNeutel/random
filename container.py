10db22c4b1984f95b4fe05639e6752c04456ac17
SanderNeutel <sanderneutel@gmail.com>
2015-09-14 21:54:14 +0200

    ooo

--- container.py
+++ container.py
@@ -7,10 +7,10 @@ from subprocess import Popen, PIPE
 import subprocess    
     
 def getcontainer_ip(con_name):
-    #get_con="lxc-info -n " + str(con_name) + " | grep IP | cut -d\' \' -f2-"
-    #p = Popen(get_con, shell=True, stdout=PIPE)
-    #output= p.stdout.read()
-    #container_ip = output.lstrip()
-    print "container_ip"
+    get_con="lxc-info -n " + str(con_name) + " | grep IP | cut -d\' \' -f2-"
+    p = Popen(get_con, shell=True, stdout=PIPE)
+    output= p.stdout.read()
+    container_ip = output.lstrip()
+    print container_ip
     return container_ip
         
