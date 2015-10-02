86dbb05e0f28bc2faab33944908cb275f12529c7
SanderNeutel <sanderneutel@gmail.com>
2015-10-03 00:05:58 +0200

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pkgutil
-
-# this is the package we are inspecting -- for example 'email' from stdlib
-import container
-
-package = container
-for importer, modname, ispkg in pkgutil.iter_modules(package.__path__):
-    print "Found submodule %s (is a package: %s)" % (modname, ispkg)
